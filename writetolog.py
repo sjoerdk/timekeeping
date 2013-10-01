d22ad70f095a7081223fe0330e89db88b09d61bd
sjoerdk <w.s.kerkstra@gmail.com>
2013-10-01 11:50:18 +0200

    Working clean python version of logging script. No google doc interface yet

---
+++ writetolog.py
@@ -0,0 +1,67 @@
+import pdb
+import os
+from datetime import datetime
+from optparse import OptionParser
+
+"""
+31/04/2013 Sjoerd
+
+Write datetime + logmessage. To be used with launchy so I can write log messages
+with one keystroke. Write to Google docs directly.
+
+"""
+
+
+
+
+
+#====== main automation ========================================================
+
+
+def write_msg_to_file(msg,filename):
+    """ Write "datetime - message" to new line of file
+
+    """    
+    f = open(filename,"a+")    
+    try:
+        now = datetime.now().strftime("%Y%m%d %H:%M:%S")    
+        f.write('%s - "%s"\n'%(now, msg))        
+    finally:
+        f.close()
+        
+    
+    
+def init_optparse():
+    """ read options from commandline
+    """
+    
+    parser = OptionParser("Usage: %prog [options] message (string)")            
+    parser.add_option("-d", "--destination", type="choice",
+                      choices = ["file","googledoc"],
+                      dest="destination", default="file",
+                      help='Write message to this destination. Choices: ["file","googledoc"]')
+
+    return parser
+    
+
+def mainloop():
+    parser = init_optparse()
+    (options, args) = parser.parse_args()
+    
+    if len(args) != 1:
+        print ("Expected 1 argument: the message to write. instead found %d.\n" %len(args))
+        parser.print_help()
+    
+    if options.destination == "file":
+        write_msg_to_file(args[0],"timing_test.log")
+
+    elif options.destination == "googledoc":
+        raise NotImplementedError ("make this!")
+
+
+    
+mainloop()
+    
+    
+
+
