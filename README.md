73fc32d9b826b308c78c64e04d66de4df2a3a2f3
sjoerdk <w.s.kerkstra@gmail.com>
2014-02-05 15:15:35 +0100

    Can now write date and msg into arbitrary columns, cleaned code a bit, updated readme to include timeshift parameter

--- README.md
+++ README.md
@@ -1,26 +1,34 @@
 timekeeping
 ===========
-
 Write messages to a log with one keystroke.
 
-= Required packages =
+# Requirements
+## python packages 
 * gspread 0.1.0 for writing to google docs
 
-= setup =
-== for writing to local file == 
-Writing to log file works out of the box:
->> python write_to_log.py "a message"
+## programs
+* launchy (http://www.launchy.net/) launch manager, to run the python script which does the actual logging in a single keystroke. You can use any launch manager of method you want of course. Launchy works for me.
 
-== for writing to google docs ==
+# setup 
+## for writing to local file
+Writing to log file works out of the box:
+> python write_to_log.py "a message"
+Or to write a message and set the time to 30 minutes ago
+> python write_to_log.py "a message" -t -30
+
+# for writing to google docs
 * install gspread using 'pip install gspread'
 * edit write_to_log.cfg to contain your google credentials and spreadsheet
+* then call it like this:
+> python write_to_log.py "a message" -d googledoc
 
-== setup in launchy ==
+# setup in launchy
 * download and install launchy (http://www.launchy.net/download.php)
 * in launchy settings go to 'plugins' tab and select "Runner" on the right
 * In runner click '+' on the bottom and add (omit parenthesis): 
 ** Name : 'log'
 ** Program: 'C:/path/to/write_to_log_folder/write_tot_google_doc.bat' (put correct path here, .bat is needed because I could net get launchy te launch python directly)
-** Arguments: '"$$"'  (this means 'first argument'. In launchy you can enter the first argument after pressing tab.
+** Arguments: '"$$" $$'  (this means 'first argument'. In launchy you can enter the first argument after pressing tab.
 * To log something, open up launchy (alt-space by default) and type "log",<tab>,"some message"
+* To log something to a time 10 minutes ago, open launchy and type "log",<tab>,"some message"<tab>-10
 
--- write_to_log.py
+++ write_to_log.py
@@ -1,8 +1,10 @@
 import pdb
 import os
-from datetime import datetime
-from optparse import OptionParser
 import ConfigParser
+from datetime import datetime,timedelta
+from optparse import OptionParser
+
+
 
 try:
     import gspread
@@ -19,20 +21,8 @@ with one keystroke. Write to Google docs directly.
 """
 
 
-def get_now_string():
-    return datetime.now().strftime("%Y%m%d %H:%M:%S")    
-    
 
-def write_msg_to_file(msg,filename):
-    """ Write "datetime - message" to new line of file
 
-    """    
-    f = open(filename,"a+")    
-    try:
-        nowstr = get_now_string()        
-        f.write('%s - "%s"\n'%(nowstr, msg)        ) 
-    finally:
-        f.close()
         
 
 def init_optparse():
@@ -43,59 +33,152 @@ def init_optparse():
     parser.add_option("-d", "--destination", type="choice",
                       choices = ["file","googledoc"],
                       dest="destination", default="file",
-                      help='Write message to this destination. Choices: ["file","googledoc"]')
+                      help='Write message to this destination, configured in "write_to_log.cfg" Choices: ["file","googledoc"]')
+    parser.add_option("-t", "--time_shift", dest="time_shift",default=0,
+                      help='Add this many minutes to the date written. Number can be negative')
 
     return parser
 
-    
+
+def create_log_item(msg,options):
+    time_shift = options.time_shift
+    if time_shift == "" or time_shift == None:
+        time_shift = 0              
+    log_item = LogItem(msg,int(time_shift))    
+    return log_item
+
+
 
 def mainloop(configfile):
     parser = init_optparse()
     (options, args) = parser.parse_args()
-    
+            
     if len(args) != 1:
         print ("Expected 1 argument: the message to write. instead found %d.\n" %len(args))
         parser.print_help()
 
     if options.destination == "file":
-        write_msg_to_file(args[0],"timing_test.log")
+        filename = "timing_test.log"
+        writer = FileLogWriter(filename)            
 
-    elif options.destination == "googledoc":
+    elif options.destination == "googledoc":        
+        writer = GoogleDocLogWriter(configfile)
+    
+    msg = args[0]
+    writer.write(create_log_item(msg,options))
+
+
+
+class LogWriter:
+    """ Writes date and message to a log just for inheritance.
+    """
+
+    def write(self,log_item):
+        raise NotImplemented("This method should be implemented in inheriting classes")
+        
+
+class FileLogWriter(LogWriter):
+    
+    def __init__(self,filename):
+        self.filename = filename
+    
+    def write(self,log_item):
+        """ Write "datetime - message" to new line of file            
+        """    
+        f = open(self.filename,"a+")    
+        try:            
+            f.write(str(log_item)) 
+        finally:
+            f.close()
+
+class GoogleDocLogWriter(LogWriter):
+
+    def __init__(self,configfile):
+        self.parse(configfile)        
+
+
+    def parse(self,configfile):
         try:
             gspread
         except NameError:
             raise Exception("gspread lib could not be imported. Writing to google doc has"
                             "been disabled")
 
-        if not os.path.exists(configfile):
-            raise Exception("Configfile could not be found at %s" %configfile)
-            
-        config = ConfigParser.RawConfigParser()        
+        config = ConfigParser.RawConfigParser()
         config.read(configfile)
         
-        google_spreadsheet_name = config.get('google_spreadsheet','google_spreadsheet_name')
-        google_id = config.get('google_spreadsheet','google_id')
-        google_password = config.get('google_spreadsheet','google_password')
+        self.google_spreadsheet_name = config.get('google_spreadsheet','google_spreadsheet_name')
+        self.google_id = config.get('google_spreadsheet','google_id')
+        self.google_password = config.get('google_spreadsheet','google_password')
+
+        self.google_timestring_column = self.get_config_or_default(config,'google_timestring_column',"A")
+        self.google_message_column = self.get_config_or_default(config,'google_message_column',"B")
+                
+
+    def get_config_or_default(self,config,optionname,default):
+        """ To allow setting of defaults if an optionname is not found in the config file
+
+        """        
+        try:
+            return config.get('google_spreadsheet',optionname)
+        except ConfigParser.NoOptionError:
+            return default
         
-        
+    def write(self,log_item):
+        worksheet = self.open_google_doc()
+        self.write_to_worksheet(log_item,worksheet)
+
+
+    def open_google_doc(self):
         # Login with your Google account
-        gc = gspread.login(google_id,google_password)
+        gc = gspread.login(self.google_id,self.google_password)
 
         # Open a worksheet from spreadsheet with one shot
-        wks = gc.open(google_spreadsheet_name).sheet1
+        worksheet = gc.open(self.google_spreadsheet_name).sheet1
+        return worksheet
 
-        highest_row =  len(wks.col_values(1))
-
-        nowstr = get_now_string()
-        msg = args[0]
-        cell_list = wks.range('A%s:B%s'%(highest_row+1,highest_row+1))
-        cell_list[0].value = nowstr
-        cell_list[1].value = msg
+    def get_next_empty_row(self,worksheet):
+        """Find the highest empty row to know where you can write a new entry
+        Only checks for first empty cell in column where timestring is written
         
-        wks.update_cells(cell_list)
+        """        
+        idx = worksheet.get_int_addr(self.google_timestring_column+"1")[1]
+        return len(worksheet.col_values(idx))+1
         
 
-configfile = r"C:\code\scripts\timekeeping\write_to_log.cfg"
+    def write_to_worksheet(self,log_item,worksheet):
+        row = self.get_next_empty_row(worksheet)
+        
+        timestring_cell = '{0}{1}'.format(self.google_timestring_column,row)
+        message_cell = '{0}{1}'.format(self.google_message_column,row)        
+        worksheet.update_acell(timestring_cell,log_item.get_time_string())
+        worksheet.update_acell(message_cell,log_item.msg)
+        
+
+class LogItem:
+    def __init__(self,msg,add_minutes=0):
+        
+        self.msg = msg        
+        self.time = self.get_time(add_minutes)
+
+    def __str__(self):
+        datestring = self.time.strftime("%Y%m%d %H:%M:%S")        
+        return '%s - "%s"\n'%(datestring, self.msg) 
+
+    def get_time_string(self):
+        return self.time.strftime("%Y%m%d %H:%M:%S")
+    
+    
+    def get_time(self,add_minutes):
+        time = datetime.now()
+        if add_minutes != 0:
+            delta = timedelta(minutes=add_minutes)
+            time = time + delta        
+        return time
+
+    
+
+configfile = "D:/code/scripts/timekeeping/write_to_log.cfg"
 mainloop(configfile)
     
     
