f5b20a362cfc4e8ccfe16805ac86da32437fee49
sjoerdk <w.s.kerkstra@gmail.com>
2014-02-05 15:18:02 +0100

    Write_to_google_doc now also incorporates timeshift

--- write_to_google_doc.bat
+++ write_to_google_doc.bat
@@ -4,5 +4,6 @@ REM launchy will not run python script with parameters directly so I need to use
 
 REM parameters 	   ========================================
 SET msg=%1
+SET time_shift=%2
 
-python write_to_log.py -d googledoc %msg%
+python write_to_log.py -d googledoc %msg% -t "%time_shift%"
